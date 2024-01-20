a62bcbc873d93b7be63478ab8e5c946e8cc51bc5
jrunge <jrunge@precisionplanting.com>
2024-01-20 15:15:05 -0600

    AST structure

--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,33 @@
+use std::any::Any;
+
 use crate::lex::Symbol;
 
 //Define abstract syntax tree
-struct AST {
+struct AST_Program {
+    functions: Vec<AST_Function>,
+}
 
+struct AST_Function {
+    name: String,
+    return_type: String,
+    statements: Vec<AST_Statement>,
+}
+
+struct AST_Statement {
+    action: Statement_Action,
+    expression: AST_Expression,
+}
+
+struct AST_Expression {
+    value: Any,
+}
+
+enum Statement_Action {
+    Return,
+    If,
+    While,
+    For,
+    Assign,
 }
 
 #[derive(Debug)]
@@ -13,7 +38,7 @@ struct Grammar {
     expression: Vec<String>,
 }
 
-pub fn parse(tokens: Vec<(Symbol, String)>) {
+pub fn parse(tokens: Vec<(Symbol, String)>) -> AST_Program {
     let grammar = get_grammar();
 
     println!("Grammar: {:?}", grammar);
@@ -26,6 +51,10 @@ pub fn parse(tokens: Vec<(Symbol, String)>) {
             _ => (),
         }
     }
+
+    return AST_Program{
+        functions: Fns,
+    };
 }
 
 fn get_grammar() -> Grammar {
@@ -50,16 +79,15 @@ fn get_grammar() -> Grammar {
 
         let terminals = s.next().expect("There was an error parsing the grammar file.");
         let terminals: Vec<String> = terminals
-            .trim()
             .split("|")
-            .map(|s| s.to_string())
+            .map(|s| s.trim().to_string())
             .collect();
 
         match rule {
             "<program>" => grammar.program = terminals.clone(),
             "<function>" => grammar.function = terminals.clone(),
             "<statement>" => grammar.statement = terminals.clone(),
-            "<expression>" => grammar.expression = terminals.clone(),
+            "<exp>" => grammar.expression = terminals.clone(),
             _ => (),
         };
     }
@@ -67,6 +95,19 @@ fn get_grammar() -> Grammar {
     grammar
 }
 
-fn parse_terminal(t: String) {
-    
+fn parse_statement(s: String) {
+    // tok = tokens.next()
+    // if tok.type != "RETURN_KEYWORD":
+    //     fail()
+    // tok = tokens.next()
+    // if tok.type != "INT"
+    //     fail()
+    // exp = parse_exp(tokens) //parse_exp will pop off more tokens
+    // statement = Return(exp)
+
+    // tok = tokens.next()
+    // if tok.type != "SEMICOLON":
+    //     fail()
+
+    // return statement
 }
